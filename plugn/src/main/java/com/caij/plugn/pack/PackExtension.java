46f549d72ace069b6ec59c5e5362f93b70d714fb
Caij <271945881@qq.com>
2019-03-01 14:57:24 +0800

    Initial commit

---
+++ plugn/src/main/java/com/caij/plugn/pack/PackExtension.java
@@ -0,0 +1,12 @@
+package com.caij.plugn.pack;
+
+public class PackExtension {
+
+    public String walleJarPath;
+    public String outFileDir;
+    public String channelFilePath;
+
+    public boolean isApkCanary;
+    public String  apkCanaryJarPath;
+    public String apkCanaryJsonPath;
+}
